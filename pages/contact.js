690b3c2bc510e0e2d9d57b9e8fa5e6624cb0288d
Asim84q <Asim84q>
2021-08-05 10:06:54 +0400

    kk

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,44 @@
-import React from "react";
+import { useState } from "react";
 
 export default function contact() {
+  let [count, setCount] = useState(0);
   return (
     <div>
-      <h1>A Contact Page</h1>
+      <head>
+        <title>A Contact Page</title>
+      </head>
+
+      <div className="container">
+        <main>
+          <p className="description">
+            Get started by editing <code>pages/index.js</code>
+          </p>
+          <div>
+            This is the counter: {count}
+            <p>
+              <button
+                onClick={() => {
+                  setCount(count + 1);
+                }}
+              >
+                Add
+              </button>
+              <button
+                onClick={() => {
+                  setCount(count - 1);
+                }}
+              >
+                Subtract
+              </button>
+            </p>
+            <form>
+              <label htmlFor="name">Name</label>
+              <input id="name" type="text" autoComplete="name" required />
+              <button type="submit">Register</button>
+            </form>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my next world!" />
+        <Header title="Welcome to my next world2!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
